f9189966879da1efaad6454b7fd3ed4235c05a44
Sagrika21goel <sagrika.goel5@gmail.com>
2018-10-19 20:35:17 +0530

    application routes added

--- test-paper/app.js
+++ test-paper/app.js
@@ -1,1 +1,8 @@
 var app = angular.module('quiz', ['ngRoute']);
+app.config(function($routeProvider)
+ { $routeProvider 
+    .when('/', { templateUrl : 'start_test.html', controller : 'StartTestController' }) 
+    .when('/show', { templateUrl : 'show_test.html', controller : 'ShowTestController' }) 
+    .when('/summary', { templateUrl : 'summary.html', controller : 'SummaryController' }) 
+    .otherwise({redirectTo: '/'}); 
+});
